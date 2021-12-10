d29d1c6f1666b6c5790a1be2893c39a2da3edc5b
AnVT <anvt96@gmail.com>
2021-12-10 09:19:55 +0700

    filter role, udpate UI

--- src/components/HomeComponent/index.jsx
+++ src/components/HomeComponent/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import UserItem from "../UserItem";
-import Modal from "react-modal";
-// import ModalComponent from "../ModalComponent"
+// import Modal from "react-modal";
+
+import { Button, Table, Input, Modal, ModalHeader, } from "reactstrap";
 
 const listUsers = [
   {
@@ -34,49 +35,51 @@ const initUser = {
   name: "",
   email: "",
   role: "",
-  isEdit: false
+  isEdit: false,
 };
 
-Modal.setAppElement("#root");
+// Modal.setAppElement("#root");
+
 function Home() {
   const [userInput, setUserInput] = useState(initUser);
- // console.log(userInput);
+  // console.log(userInput);
 
   const [users, setUsers] = useState(listUsers);
-  const [list, setList] = useState(listUsers)
+  const [list, setList] = useState(listUsers);
   const [modal, setModal] = useState(false);
   //const [isUpdate, setIsUpdate] = useState(false)
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null);
   const clickOnModal = () => {
     setModal(!modal);
   };
 
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState("");
+
+  const [role, setRole] = useState("");
 
   const submitForm = () => {
     const newUserList = [...users];
-    const filterData = newUserList.filter((item) => item.id === currentUser)
-    if(currentUser){
+    const filterData = newUserList.filter((item) => item.id === currentUser);
+    if (currentUser) {
+      //tìm ra id để update
       const index = newUserList.indexOf(filterData[0]);
-      console.log("index",index,  newUserList[index], newUserList)
+      console.log("index", index, newUserList[index], newUserList);
       newUserList[index] = userInput;
-      console.log("newlistuser", newUserList)
-      setUserInput(newUserList)
-      setUsers(newUserList)
-      setList(newUserList)
-    }else{
-     
-      console.log("test",newUserList[newUserList.length-1])
-      newUserList.push({...userInput, id: users[users.length-1].id+1});
+      console.log("newlistuser", newUserList);
+      setUserInput(newUserList);
+      setUsers(newUserList);
+      setList(newUserList);
+    } else {
+      // add user
+      console.log("test", newUserList[newUserList.length - 1]);
+      newUserList.push({ ...userInput, id: users[users.length - 1].id + 1 });
       console.log(newUserList, userInput);
       setUsers(newUserList);
-      setUserInput(initUser)
-      setCurrentUser(null)
-      setList(newUserList)
-      
+      setUserInput(initUser);
+      setCurrentUser(null);
+      setList(newUserList);
     }
-    clickOnModal()
-
+    clickOnModal();
   };
 
   const handleChangeInput = (e) => {
@@ -86,15 +89,15 @@ function Home() {
   };
 
   const handleUpdate = (id, name) => {
-    setCurrentUser(id)
-    
+    setCurrentUser(id);
+
     const newUserList = [...users];
-    const filterData = newUserList.filter((item) => item.id === id)
+    const filterData = newUserList.filter((item) => item.id === id);
+    // console.log('=====',filterData)
+    setUserInput({ ...filterData[0], isEdit: true });
+    clickOnModal();
 
-    setUserInput({...filterData[0], isEdit: true})
-    clickOnModal()
-
-    console.log(filterData)
+    console.log(filterData);
     console.log("update id:", id + " có tên user: ", name);
   };
 
@@ -109,117 +112,123 @@ function Home() {
     });
     setList((prev) => {
       return prev.filter((item) => item.id !== userList.id);
-    })
+    });
   };
 
   const handleSearch = (event) => {
-    const text = event.target.value
-    
-    setSearchText(text)
+    const text = event.target.value;
+
+    setSearchText(text);
     const newUserList = [...list];
-    console.log("aaa",text, newUserList)
-    const filterData = newUserList.filter((item) => item.name.toLowerCase().includes(text.toLowerCase()))
-    console.log("filterData", filterData)
-    setUsers(filterData)
-  }
+    console.log("aaa", text, newUserList);
+    const filterData = newUserList.filter((item) =>
+      item.name.toLowerCase().includes(text.toLowerCase())
+    );
+    console.log("filterData", filterData);
+    setUsers(filterData);
+  };
+
+  const handleSelect = (e) => {
+    const roleSelected = e.target.value;
+    const newUserList = [...list];
+    setRole(roleSelected);
+    const selectRole = newUserList.filter((item) => item.role === roleSelected);
+    if (roleSelected === "") {
+      setUsers(newUserList);
+    } else {
+      setUsers(selectRole);
+    }
+    //setList(selectRole)
+    console.log(selectRole);
+  };
+
   return (
     <div style={{ padding: "30px" }}>
-        <input
-          name="name"
-          value={userInput.name}
-          onChange={handleChangeInput}
-        />
-        <input
-          name="email"
-          value={userInput.email}
-          onChange={handleChangeInput}
-        />
-        <input
-          name="role"
-          value={userInput.role}
-          onChange={handleChangeInput}
-        />
-        <button onClick={()=>submitForm()}>{userInput.isEdit ? 'Update' : 'Add'}</button>
+      <Button
+        style={{ verticalAlign: "baseline", marginRight: "150px" }}
+        color="primary"
+        onClick={clickOnModal}
+      >
+        Add User
+      </Button>
+      <Input
+        style={{ width: "50%", display: "inline-block" }}
+        type="text"
+        name="test"
+        value={searchText}
+        placeholder="Search by name"
+        onChange={handleSearch}
+      />
 
-
-      <table border="1">
+      <Table hover>
         <thead>User Info</thead>
-        <input
-          type="text"
-          name="test"
-              value={searchText} 
-              placeholder='search'
-              onChange={handleSearch} />
-        <button
-          style={{ backgroundColor: "blue", color: "white" }}
-          onClick={clickOnModal}
-        >
-          Add User
-        </button>
-        <Modal
-          isOpen={modal}
-          onRequestClose={clickOnModal}
-          style={{
-            overlay: {
-              backgroundColor: "grey",
-            },
-            content: {
-              color: "orange",
-            },
-          }}
-        >
-          {/* <form> */}
-            <table>
-              <tbody>
-                <tr>
-                  <th>
-                    <label>Name</label>
-                  </th>
-                  <td>
-                    <input
-                      name="name"
-                      value={userInput.name}
-                      onChange={handleChangeInput}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <label>Email</label>
-                  </th>
-                  <td>
-                    <input
-                      name="email"
-                      value={userInput.email}
-                      onChange={handleChangeInput}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <label>Role</label>
-                  </th>
-                  <td>
-                    <input
-                      name="role"
-                      value={userInput.role}
-                      onChange={handleChangeInput}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <button onClick={submitForm}>
-              Add
-            </button>
-          {/* </form> */}
-          <button onClick={clickOnModal}>Close</button>
+        <Modal isOpen={modal} onRequestClose={clickOnModal}>
+          <ModalHeader toggle={clickOnModal}>
+            Add User
+          </ModalHeader>
+          <Table>
+            <tbody>
+              <tr>
+                <th>
+                  <label>Name</label>
+                </th>
+                <td>
+                  <Input
+                    className="col-3"
+                    name="name"
+                    value={userInput.name}
+                    onChange={handleChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <label>Email</label>
+                </th>
+                <td>
+                  <Input
+                    className="col-3"
+                    name="email"
+                    value={userInput.email}
+                    onChange={handleChangeInput}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <label>Role</label>
+                </th>
+                <td>
+                  <Input
+                    className="col-3"
+                    name="role"
+                    value={userInput.role}
+                    onChange={handleChangeInput}
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          <Button style={{margin:'15px'}} color="primary" onClick={submitForm}>
+            Add
+          </Button>
+          {/* <Button onClick={clickOnModal}>
+            Close
+          </Button> */}
         </Modal>
         <tr>
           <th>ID</th>
           <th>Name</th>
           <th>Email</th>
-          <th>Role</th>
+          <th>Role
+            <select value={role} onChange={handleSelect} style={{marginLeft:'20px'}}>
+              <option value="">...</option>
+              <option value="ReactJS">ReactJS</option>
+              <option value="NodeJS">NodeJS</option>
+              <option value="Javascript">Javascript</option>
+              <option value="TypeScript">TypeScript</option>
+            </select>
+          </th>
           <th>Update</th>
           <th>Delete</th>
         </tr>
@@ -234,7 +243,7 @@ function Home() {
             onClickDelete={() => handleDelete(user)}
           />
         ))}
-      </table>
+      </Table>
     </div>
   );
 }
